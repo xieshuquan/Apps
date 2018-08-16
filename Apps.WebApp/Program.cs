3be1a5cd0649c194aa2764647bf902e93b1efb2c
谢树权(xieshuquan) <xieshuquan@tuhu.cn>
2018-08-16 17:11:19 +0800

    迁移目录

--- Apps.WebApp/Program.cs
+++ Apps.WebApp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebApp
+namespace Apps.WebApp
 {
     public class Program
     {
--- Apps.WebApp/Startup.cs
+++ Apps.WebApp/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WebApp
+namespace Apps.WebApp
 {
     public class Startup
     {
