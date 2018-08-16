fad55e8f6b338a9696f85deab183373015b973fa
谢树权(xieshuquan) <xieshuquan@tuhu.cn>
2018-08-16 15:51:00 +0800

    init v2

---
+++ Utils/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IgnoreCaseEquals(this string a, string b)
+        {
+            return string.Equals(a, b, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
